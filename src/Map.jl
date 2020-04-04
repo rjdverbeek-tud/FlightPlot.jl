f569ef96c6b7bcd00a560a568cac0877c62596a6
rjdverbeek <rjdverbeek@gmail.com>
2020-04-04 20:54:19 +0200

    Refactor

--- src/Map.jl
+++ src/Map.jl
@@ -1,20 +1,66 @@
-const GridColor = RGB(107.0/255.0, 191.0/255.0, 91.0/255.0)
-const CoastColor = RGB(117.0/255.0, 207.0/255.0, 224.0/255.0)
-const SeaColor = RGB(205.0/255.0, 235.0/255.0,240.0/255.0)
-const LandColor = RGB(1.0, 1.0, 1.0)
+# const GridColor = RGB(107.0/255.0, 191.0/255.0, 91.0/255.0)
+# const CoastColor = RGB(117.0/255.0, 207.0/255.0, 224.0/255.0)
+# const SeaColor = RGB(205.0/255.0, 235.0/255.0,240.0/255.0)
+# const LandColor = RGB(1.0, 1.0, 1.0)
 
-function add_map!(shp::String, projparam=Equirectangular();
-    linecolor::RGB{Float64} = CoastColor, fillcolor::RGB{Float64} = LandColor,
-    background_color::RGB{Float64} = SeaColor, kwargs...)
-
-    add_polygon!(shp, projparam, linecolor=linecolor, fill = (0, 1.0, fillcolor),
-    background_color = background_color; kwargs...)
+struct MapSettings
+    map_filename::String
+    lake_filename::String
+    projection::Union{Equirectangular, Mercator, Lambert, Gnomonic}
+    xticks::StepRangeLen
+    yticks::StepRangeLen
+    gridcolor::RGB{Float64}
+    coastcolor::RGB{Float64}
+    seacolor::RGB{Float64}
+    landcolor::RGB{Float64}
+    gridlinewidth::Float64
+    gridalpha::Float64
+    function MapSettings(;map_filename::String="data\\GSHHS_c_L1.shp",
+        lake_filename::String="data\\GSHHS_c_L2.shp",
+        projection::Union{Equirectangular, Mercator, Lambert, Gnomonic}=
+        Equirectangular(), xticks::StepRangeLen=-180.0:10.0:180.0,
+        yticks::StepRangeLen=-85.0:2.5:85.0,
+        gridcolor::RGB{Float64} = RGB(107.0/255.0, 191.0/255.0, 91.0/255.0),
+        coastcolor::RGB{Float64} = RGB(117.0/255.0, 207.0/255.0, 224.0/255.0),
+        seacolor::RGB{Float64} = RGB(205.0/255.0, 235.0/255.0,240.0/255.0),
+        landcolor::RGB{Float64} = RGB(1.0, 1.0, 1.0),
+        gridlinewidth::Float64 = 0.01, gridalpha::Float64 = 0.33)
+        new(map_filename, lake_filename, projection, xticks, yticks, gridcolor,
+        coastcolor, seacolor, landcolor, gridlinewidth, gridalpha)
+    end
 end
+
+function add_map!(shp::String, projparam=Equirectangular(); kwargs...)
+    add_polygon!(shp, projparam; kwargs...)
+end
+
+add_map!(mapsettings::MapSettings; kwargs...) = add_map!(mapsettings.map_filename,
+mapsettings.projection; linecolor=mapsettings.coastcolor,
+fillcolor=mapsettings.landcolor, background_color = mapsettings.seacolor,
+fillalpha=1.0, fillrange=0,
+kwargs...)
 
 function add_lakes!(shp::String, projparam=Equirectangular();
     linecolor::RGB{Float64} = CoastColor, fillcolor::RGB{Float64} = SeaColor,
     kwargs...)
 
-    add_polygon!(shp, projparam, linecolor=linecolor, fill = (0, 1.0, fillcolor);
-    kwargs...)
+    add_polygon!(shp, projparam; kwargs...)
 end
+
+add_lakes!(mapsettings::MapSettings; kwargs...) = add_map!(
+mapsettings.lake_filename, mapsettings.projection;
+linecolor=mapsettings.coastcolor, fillcolor=mapsettings.seacolor,
+background_color = mapsettings.seacolor, fillalpha=1.0, fillrange=0, kwargs...)
+
+function add_grid!(mapsettings::MapSettings; kwargs...)
+    plot!(mapsettings.xticks, mapsettings.yticks, mapsettings.projection;
+    linewidth=mapsettings.gridlinewidth, linecolor=mapsettings.gridcolor,
+    linealpha = mapsettings.gridalpha)
+end
+
+# function xlims_map!(mapsettings::MapSettings,
+#     xlims::Tuple{Float64, Float64} = (-180.0, 180.0))
+#     left = MapSettings.projection
+#     right = xlims[2]
+#     plot!(xlims=)
+# end
--- src/Plot.jl
+++ src/Plot.jl
@@ -17,9 +17,11 @@ gr()
 
 function initialize_plot()
     plot(legend = false, grid = false, showaxis = false, ticks = false,
-    framestyle=:box, xticks = -180.0:10.0:180.0, yticks = -85.0:2.5:85.0)
-    plot!(showaxis = true, ticks = true, xticks = -180.0:10.0:180.0,
-    yticks = -85.0:2.5:85.0, xlim=(-40.0,40.0), ylim=(-20.0, 20.0))
+    framestyle=:box)
+    # plot(legend = false, grid = false, showaxis = false, ticks = false,
+    # framestyle=:box, xticks = -180.0:10.0:180.0, yticks = -85.0:2.5:85.0)
+    # plot!(showaxis = true, ticks = true, xticks = -180.0:10.0:180.0,
+    # yticks = -85.0:2.5:85.0, xlim=(-40.0,40.0), ylim=(-20.0, 20.0))
 end
 
 function shapes2lonlat(shapes::Array{Union{Missing, Shapefile.Polygon},1})
@@ -36,42 +38,33 @@ function shapes2lonlat(shapes::Array{Union{Missing, Shapefile.Polygon},1})
     lon, lat
 end
 
-# function limitdistance(centre_point_lon_deg::Float64,
-#     centre_point_lat_deg::Float64, lon_deg::Float64, lat_deg::Float64)
-#     center_point = Navigation.Point_deg(centre_point_lat_deg,
-#     centre_point_lon_deg)
-#     point = Navigation.Point_deg(lat_deg, lon_deg)
-#     angular_distance = Navigation.distance(center_point, point, 1.0)
-#     max_angular_distance = π * 0.4
-#     if angular_distance > max_angular_distance
-#         fraction = max_angular_distance / angular_distance
-#         new_point = Navigation.intermediate_point(center_point, point, fraction)
-#         return [new_point.λ new_point.ϕ]
-#     else
-#         return [lon_deg lat_deg]
-#     end
-# end
-
 function grid2lonlat(grid_lon_deg::StepRangeLen, grid_lat_deg::StepRangeLen)
     lon = Vector{Float64}()
     lat = Vector{Float64}()
     lon_prev = NaN
     lat_prev = NaN
     fractions = 2
-    fraction_section = 1.0 / fractions
+    function grid_loop(grid_lon::Float64, grid_lat::Float64,
+        lon_prev::Float64, lat_prev::Float64, lon::Vector{Float64},
+        lat::Vector{Float64}, fractions::Int64)
+        if !isnan(lon_prev)
+            for part in 1:(fractions - 1)
+                fraction = 1.0 / fractions * part
+                append!(lon, lon_prev + (grid_lon - lon_prev) * fraction)
+                append!(lat, lat_prev + (grid_lat - lat_prev) * fraction)
+            end
+        end
+        append!(lon, grid_lon)
+        append!(lat, grid_lat)
+        lon_prev = grid_lon
+        lat_prev = grid_lat
+        lon, lat, lon_prev, lat_prev
+    end
+
     for grid_lat in grid_lat_deg
         for grid_lon in grid_lon_deg
-            if !isnan(lon_prev)
-                for part in 1:(fractions - 1)
-                    fraction = fraction_section * part
-                    append!(lon, lon_prev + (grid_lon - lon_prev) * fraction)
-                    append!(lat, lat_prev + (grid_lat - lat_prev) * fraction)
-                end
-            end
-            append!(lon, grid_lon)
-            append!(lat, grid_lat)
-            lon_prev = grid_lon
-            lat_prev = grid_lat
+            lon, lat, lon_prev, lat_prev = grid_loop(grid_lon, grid_lat,
+            lon_prev, lat_prev, lon, lat, fractions)
         end
         append!(lon, NaN)
         append!(lat, NaN)
@@ -80,17 +73,8 @@ function grid2lonlat(grid_lon_deg::StepRangeLen, grid_lat_deg::StepRangeLen)
     end
     for grid_lon in grid_lon_deg
         for grid_lat in grid_lat_deg
-            if !isnan(lon_prev)
-                for part in 1:(fractions - 1)
-                    fraction = fraction_section * part
-                    append!(lon, lon_prev + (grid_lon - lon_prev) * fraction)
-                    append!(lat, lat_prev + (grid_lat - lat_prev) * fraction)
-                end
-            end
-            append!(lon, grid_lon)
-            append!(lat, grid_lat)
-            lon_prev = grid_lon
-            lat_prev = grid_lat
+            lon, lat, lon_prev, lat_prev = grid_loop(grid_lon, grid_lat,
+            lon_prev, lat_prev, lon, lat, fractions)
         end
         append!(lon, NaN)
         append!(lat, NaN)
@@ -157,22 +141,3 @@ end
 function add_polygon!(shp::String, projparam=Equirectangular(); kwargs...)
     plot!(get_shapefile(shp), projparam; kwargs...)
 end
-
-function add_grid!(plt::Plots.Plot, projparam=Equirectangular();gridlinewidth = 0.03,
-    foreground_color_grid::RGB{Float64} = GridColor, kwargs...)
-    plot!(gridlinewidth = gridlinewidth,
-    foreground_color_grid = foreground_color_grid, kwargs...)
-    if typeof(projparam) == Gnomonic
-        projparam.center_lon_deg
-        plot!(xticks = (-90.0 + projparam.center_lon_deg):10.0:(90.0 +
-        projparam.center_lon_deg))
-    end
-    plot!(plt[1].attr[:xaxis][:ticks], plt[1].attr[:yaxis][:ticks], projparam;
-    linewidth=gridlinewidth, linecolor=foreground_color_grid, linealpha = 0.5)
-end
-
-# function add_axis!(plt::Plots.Plot, projparam=Equirectangular())
-#     lon = []
-#     lat = []
-#     plot!()
-# end
