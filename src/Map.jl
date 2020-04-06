9b538b5994752bcaa09175170f7a9874651ee1dd
rjdverbeek <rjdverbeek@gmail.com>
2020-04-06 11:57:32 +0200

    Refactor

--- src/Map.jl
+++ src/Map.jl
@@ -1,14 +1,17 @@
+using Navigation
+using Printf
 # const GridColor = RGB(107.0/255.0, 191.0/255.0, 91.0/255.0)
 # const CoastColor = RGB(117.0/255.0, 207.0/255.0, 224.0/255.0)
 # const SeaColor = RGB(205.0/255.0, 235.0/255.0,240.0/255.0)
 # const LandColor = RGB(1.0, 1.0, 1.0)
+# using Printf
 
 struct MapSettings
     map_filename::String
     lake_filename::String
     projection::Union{Equirectangular, Mercator, Lambert, Gnomonic}
-    xticks::StepRangeLen
-    yticks::StepRangeLen
+    lonticks::StepRangeLen
+    latticks::StepRangeLen
     gridcolor::RGB{Float64}
     coastcolor::RGB{Float64}
     seacolor::RGB{Float64}
@@ -18,14 +21,14 @@ struct MapSettings
     function MapSettings(;map_filename::String="data\\GSHHS_c_L1.shp",
         lake_filename::String="data\\GSHHS_c_L2.shp",
         projection::Union{Equirectangular, Mercator, Lambert, Gnomonic}=
-        Equirectangular(), xticks::StepRangeLen=-180.0:10.0:180.0,
-        yticks::StepRangeLen=-85.0:2.5:85.0,
+        Equirectangular(), lonticks::StepRangeLen=-180.0:10.0:180.0,
+        latticks::StepRangeLen=-85.0:2.5:85.0,
         gridcolor::RGB{Float64} = RGB(107.0/255.0, 191.0/255.0, 91.0/255.0),
         coastcolor::RGB{Float64} = RGB(117.0/255.0, 207.0/255.0, 224.0/255.0),
         seacolor::RGB{Float64} = RGB(205.0/255.0, 235.0/255.0,240.0/255.0),
         landcolor::RGB{Float64} = RGB(1.0, 1.0, 1.0),
         gridlinewidth::Float64 = 0.01, gridalpha::Float64 = 0.33)
-        new(map_filename, lake_filename, projection, xticks, yticks, gridcolor,
+        new(map_filename, lake_filename, projection, lonticks, latticks, gridcolor,
         coastcolor, seacolor, landcolor, gridlinewidth, gridalpha)
     end
 end
@@ -45,40 +48,98 @@ linecolor=mapsettings.coastcolor, fillcolor=mapsettings.seacolor,
 background_color = mapsettings.seacolor, fillalpha=1.0, fillrange=0)
 
 function add_grid!(mapsettings::MapSettings)
-    plot!(mapsettings.xticks, mapsettings.yticks, mapsettings.projection;
+    plot!(mapsettings.lonticks, mapsettings.latticks, mapsettings.projection;
     linewidth=mapsettings.gridlinewidth, linecolor=mapsettings.gridcolor,
     linealpha = mapsettings.gridalpha)
 end
 
+function boundaries_map(mapsettings::MapSettings;
+    lonlims::Tuple{Float64, Float64} = (-180.0, 180.0),
+    latlims::Tuple{Float64, Float64} = (-85.0, 85.0))
+    left = projection(lonlims[1], (latlims[1] + latlims[2])/2.0, mapsettings.projection)[1]
+    right = projection(lonlims[2], (latlims[1] + latlims[2])/2.0, mapsettings.projection)[1]
+    top = projection((lonlims[1] + lonlims[2])/2.0, latlims[2], mapsettings.projection)[2]
+    bottom = projection((lonlims[1] + lonlims[2])/2.0, latlims[1], mapsettings.projection)[2]
+    left, right, bottom, top
+end
+
 function lims_map!(mapsettings::MapSettings;
-    xlims::Tuple{Float64, Float64} = (-180.0, 180.0),
-    ylims::Tuple{Float64, Float64} = (-85.0, 85.0))
-    left = projection(xlims[1], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-    right = projection(xlims[2], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-    top = projection((xlims[1] + xlims[2])/2.0, ylims[2], mapsettings.projection)[2]
-    bottom = projection((xlims[1] + xlims[2])/2.0, ylims[1], mapsettings.projection)[2]
+    lonlims::Tuple{Float64, Float64} = (-180.0, 180.0),
+    latlims::Tuple{Float64, Float64} = (-85.0, 85.0))
+    left, right, bottom, top = boundaries_map(mapsettings, lonlims=lonlims,
+    latlims=latlims)
     plot!(xlims=(left, right), ylims=(bottom, top))
 end
 
 function axis_map!(mapsettings::MapSettings;
-    xlims::Tuple{Float64, Float64} = (-180.0, 180.0),
-    ylims::Tuple{Float64, Float64} = (-85.0, 85.0))
-    left = projection(xlims[1], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-    right = projection(xlims[2], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-    top = projection((xlims[1] + xlims[2])/2.0, ylims[2], mapsettings.projection)[2]
-    bottom = projection((xlims[1] + xlims[2])/2.0, ylims[1], mapsettings.projection)[2]
+    lonlims::Tuple{Float64, Float64} = (-180.0, 180.0),
+    latlims::Tuple{Float64, Float64} = (-85.0, 85.0))
+    left, right, bottom, top = boundaries_map(mapsettings, lonlims=lonlims,
+    latlims=latlims)
     x = [left, right, right, left, left]
     y = [bottom, bottom, top, top, bottom]
     plot!(x,y, linewidth=1.0, linecolor=:black)
 end
 
-#TODO Create ticks_map!
-# function ticks_map!(mapsettings::MapSettings;
-#     xlims::Tuple{Float64, Float64} = (-180.0, 180.0),
-#     ylims::Tuple{Float64, Float64} = (-85.0, 85.0))
-#     for
-#     # left = projection(xlims[1], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-#     # right = projection(xlims[2], (ylims[1] + ylims[2])/2.0, mapsettings.projection)[1]
-#     # top = projection((xlims[1] + xlims[2])/2.0, ylims[2], mapsettings.projection)[2]
-#     # bottom = projection((xlims[1] + xlims[2])/2.0, ylims[1], mapsettings.projection)[2]
-# end
+function ticks_map!(mapsettings::MapSettings;
+    lonlims::Tuple{Float64, Float64} = (-180.0, 180.0),
+    latlims::Tuple{Float64, Float64} = (-85.0, 85.0))
+
+    left, right, bottom, top = boundaries_map(mapsettings, lonlims=lonlims,
+    latlims=latlims)
+
+    #xtick Gnomonic
+    λ₃, ϕ₃ = projection_inv(left, bottom, mapsettings.projection)
+    λ₄, ϕ₄ = projection_inv(right, bottom, mapsettings.projection)
+    λ₅, ϕ₅ = projection_inv(left, top, mapsettings.projection)
+    pos₃ = Navigation.Point_deg(ϕ₃, λ₃)
+    pos₄ = Navigation.Point_deg(ϕ₄, λ₄)
+    pos₅ = Navigation.Point_deg(ϕ₅, λ₅)
+    bearing₃₄ = Navigation.bearing(pos₃, pos₄)
+
+    x = Vector{Float64}()
+    t = Vector{String}()
+    for lontick in mapsettings.lonticks
+        pos₁ = Navigation.Point_deg(latlims[2], lontick)
+        bearing₁₂ = 180.0
+        pos_tick = Navigation.intersection_point(pos₁, pos₃, bearing₁₂, bearing₃₄)
+        if !isinf(pos_tick.ϕ)
+            loc = projection(lontick, pos_tick.ϕ, mapsettings.projection)[1]
+            txt = Printf.@sprintf("%4.0f°", lontick)
+            append!(x, loc)
+            t = vcat(t, txt)
+        end
+    end
+    plot!(showaxis=true, xticks = (x,t), ticks = true)
+
+    # #ytick Gnomonic
+    # y = Vector{Float64}()
+    # t = Vector{String}()
+    #
+    # function newton(pos₁::Navigation.Point_deg, pos₂::Navigation.Point_deg,
+    #     lat_deg::Float64, left::Float64, mapsettings::MapSettings)
+    #     first_pos = pos₁
+    #     first_fraction = 0.0
+    #     last_pos = pos₂
+    #     last_fraction = 1.0
+    #     fraction = 0.5
+    #     next_pos = Navigation.intermediate_point(first_pos, last_pos, fraction)
+    #     while abs(next_pos.ϕ - lat_deg) > 0.01
+    #         if next_pos.ϕ - lat_deg > 0.0
+    #         else
+    #         end
+    #     end
+    # end
+    # #
+    # for lattick in mapsettings.latticks
+    #     if ϕ₃ ≤ lattick ≤ ϕ₅
+    #
+    #         # loc = projection(?, lattick, mapsettings.projection)[1]
+    #         txt = Printf.@sprintf("%4.0f°", lattick)
+    #         append!(y, loc)
+    #         t = vcat(t, txt)
+    #     end
+    #
+    # end
+    # plot!(yticks = (y,t))
+end
